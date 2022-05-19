9eefcd0b4e03f506caa1de2abc75e83c4277a090
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-05-19 19:10:26 -0300

    lógica de funcionamento da calculadora

---
+++ index.js
@@ -0,0 +1,49 @@
+const billValue = document.getElementById("bill_value");
+const tip = document.getElementById("tipvalue_percentage");
+const peopleValue = document.getElementById("people");
+
+let total = document.getElementById("tip_value_total");
+let tip_percentage = document.getElementById("tip_percentage");
+let tipValue = document.getElementById("tip_value");
+let numberPeople = document.getElementById("number_of_people");
+let billForPeople = document.getElementById("bill_for_people");
+let tipForPeople = document.getElementById("tip_for_people");
+let valor = 0;
+let gorjeta = 0;
+let people = 1;
+
+
+
+billValue.addEventListener("input", handleUserInput);
+tip.addEventListener("input", handleTip);
+peopleValue.addEventListener("input", qunatityOfPeople);
+
+
+function handleUserInput(evento) {
+  valor = Number(evento.target.value);
+  billTotal();
+}
+
+function handleTip(e) {
+  gorjeta = Number(e.target.value);
+  tip_percentage.textContent = `${gorjeta.toString()}%`;
+  billTotal();
+}
+
+function billTotal() {
+  let soma = valor + (valor * gorjeta) / 100;
+  total.textContent = `$${soma.toString()}`;
+  tipValue.textContent = `$${(valor * gorjeta) / 100}`;
+  billEach()
+}
+
+function qunatityOfPeople(event) {
+  people = Number(event.target.value);
+  numberPeople.textContent = `${people} people`;
+  billEach()
+};
+
+function billEach(){
+  billForPeople.textContent = (valor / people).toFixed(2)
+  tipForPeople.textContent = ((valor * gorjeta/100)/people).toFixed(2)
+}
