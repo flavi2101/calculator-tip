9db0f57b2070315f40e6bf6f877088ff89fbbc7d
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-05-20 07:23:34 -0300

    responsive desing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const billValue = document.getElementById("bill_value");
 const tip = document.getElementById("tipvalue_percentage");
 const peopleValue = document.getElementById("people");
-const resetButton = document.querySelector('button')
+const resetButton = document.querySelector("button");
+const mql = window.matchMedia("(min-widht: 600px)");
 
 let total = document.getElementById("tip_value_total");
 let tip_percentage = document.getElementById("tip_percentage");
@@ -13,12 +14,10 @@ let valor = 0;
 let gorjeta = 0;
 let people = 1;
 
-
-
 billValue.addEventListener("input", handleUserInput);
 tip.addEventListener("input", handleTip);
 peopleValue.addEventListener("input", qunatityOfPeople);
-resetButton.addEventListener('click',reset)
+resetButton.addEventListener("click", reset);
 
 
 function handleUserInput(evento) {
@@ -29,7 +28,7 @@ function handleUserInput(evento) {
 function handleTip(e) {
   gorjeta = Number(e.target.value);
   tip_percentage.textContent = `${gorjeta.toString()}%`;
-  console.dir(e)
+  console.dir(e);
   billTotal();
 }
 
@@ -37,28 +36,29 @@ function billTotal() {
   let soma = valor + (valor * gorjeta) / 100;
   total.textContent = `$${soma.toString()}`;
   tipValue.textContent = `$${(valor * gorjeta) / 100}`;
-  billEach()
+  billEach();
 }
 
 function qunatityOfPeople(event) {
   people = Number(event.target.value);
   numberPeople.textContent = `${people} people`;
-  billEach()
-};
-
-function billEach(){
-  billForPeople.textContent = (valor / people).toFixed(2)
-  tipForPeople.textContent = ((valor * gorjeta/100)/people).toFixed(2)
+  billEach();
 }
 
-function reset(){
+function billEach() {
+  billForPeople.textContent = (valor / people).toFixed(2);
+  tipForPeople.textContent = ((valor * gorjeta) / 100 / people).toFixed(2);
+}
+
+function reset() {
   billValue.value = "";
-  total.textContent ="$"
+  total.textContent = "$";
   tipValue.textContent = "$";
-  numberPeople.textContent = 'people';
+  numberPeople.textContent = "people";
   billForPeople.textContent = "$";
-  tipForPeople.textContent ="$";
-  tip.value = 0
-  peopleValue.value = 1
-  tip_percentage.textContent = "%"
+  tipForPeople.textContent = "$";
+  tip.value = 0;
+  peopleValue.value = 1;
+  tip_percentage.textContent = "%";
+  billValue.focus()
 }
